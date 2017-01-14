0bbdefe1e10784db0f29b69ae22e5ed838d7e18e
Konstantin <papaboneq@gmail.com>
2017-01-14 16:42:39 +0300

    add contdown/sandwich/.icon>(.vert/horison)

--- app/js/common.js
+++ app/js/common.js
@@ -1,26 +1,60 @@
-jQuery(document).ready(function() {
+//countdown
+jQuery(document).ready(function() {  
+  $('#countdown').countdown({
+    timestamp :  new Date(2021, 0, 1)
+  });
+  $('#countdown2').countdown({
+    timestamp :  new Date(2021, 0, 1)
+  });
+  $('#countdown-modal').countdown({
+    timestamp :  new Date(2021, 0, 1)
+  });
+});
+
+//Плавная прокрутка к якорю
+$("body").on('click', 'a[href*=#]:not([href=#])', function(e){
+  var fixed_offset = 40;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
+//Плавная прокрутка к верхнему краю последней формы заказа
+$('.to-form').click(function(e){  
+  e.preventDefault();
+  $('html, body').stop().animate({scrollTop: ($('form:visible:last').offset().top)}, 1000);
+});
 	
-  /*//header во весь экран
+/*//header во весь экран
 	jQuery(".header").css("min-height",jQuery(document).height());
+*/
 
-  //кнопка sandwiich
-	jQuery(".nav_toggle").click(function() {
-		jQuery("#sandwich").toggleClass("active");
-	});
-	
-	jQuery(".nav_mnu ul a").click(function() {
-		jQuery(".nav_mnu").fadeOut(600);
-		jQuery("#sandwich").toggleClass("active");
-	});
+//кнопка sandwiich
+jQuery(document).ready(function() {
+  sandwich.init();
+});
 
-	jQuery(".nav_toggle").click(function() {
-		if (jQuery(".nav_mnu").is(":visible")) {
-			jQuery(".nav_mnu").fadeOut(600);
-		} else {
-			jQuery(".nav_mnu").fadeIn(600);
-		};
-	});
+//Проверка кода на упаковке
+jQuery(document).ready(function() {
+  $(".chek-code button").click(function () {
+    var $code = $(".chek-code input "),
+    $message = $(".chek-code-message");
+    var $codeLenght = $(".chek-code input").val().length;
+    var codeValue = $code.val();
+    var reg1 = new RegExp("[^0-9]*", "g"), // создаем регулярку, чтобы она выбрала все цифры
+    codeValue = codeValue.replace(reg1, ''); // берем все цифры из некой переменной
+    if (codeValue.length == 15) { // Если кол-во цифр = 15
+      return $message.text("Заказанный Вами продукт является подлинным, пользуйтесь с удовольствием!");
+    }
+    else if ($code.val() !== '' && $code.val() !== ' ' && $code.val() !== '  ') {
+      return $message.text("К сожалению, данный код не найден! Вероятнее всего, вы приобрели поддельный продукт.");
+    } else {
+      return $message.text("Введите, пожалуйста, код.");
+    }
+  })
 
+});
+
+
+/*
   //initialize SWIPPER touch-slider
   var swiper01 = new Swiper('.swiper-container', {
     // Optional parameters 
@@ -54,25 +88,6 @@ jQuery(document).ready(function() {
     }
   });
   */
-  //Проверка кода на упаковке
-  $(".chek-code button").click(function () {
-    var $code = $(".chek-code input "),
-    $message = $(".chek-code-message");
-    var $codeLenght = $(".chek-code input").val().length;
-    var codeValue = $code.val();
-    var reg1 = new RegExp("[^0-9]*", "g"), // создаем регулярку, чтобы она выбрала все цифры
-    codeValue = codeValue.replace(reg1, ''); // берем все цифры из некой переменной
-    if (codeValue.length == 15) { // Если кол-во цифр = 15
-      return $message.text("Заказанный Вами продукт является подлинным, пользуйтесь с удовольствием!");
-    }
-    else if ($code.val() !== '' && $code.val() !== ' ' && $code.val() !== '  ') {
-      return $message.text("К сожалению, данный код не найден! Вероятнее всего, вы приобрели поддельный продукт.");
-    } else {
-      return $message.text("Введите, пожалуйста, код.");
-    }
-  })
-
-});
 
 
 
---
+++ app/js/sandwich.js
@@ -0,0 +1,25 @@
+var sandwich = {
+
+  toggle : function(){
+    if (jQuery(".pop-menu").is(":visible")) {
+      jQuery(".sandwich").toggleClass("active");
+      jQuery(".pop-menu").fadeOut(600);
+      document.body.style.overflowY = "scroll";
+    } else {
+      jQuery(".sandwich").toggleClass("active");
+      jQuery(".pop-menu").fadeIn(600);
+      document.body.style.overflowY = "hidden";
+    };
+  },
+
+  init: function(){
+    var popMenu = document.createElement('ul');
+    popMenu.className = 'pop-menu';
+    popMenu.innerHTML = document.querySelector('.menu>ul').innerHTML;
+    document.body.appendChild(popMenu);
+    document.querySelector('.nav-toggle').addEventListener( 'click', this.toggle);
+    popMenu.addEventListener( 'click', this.toggle);
+  }
+
+};
+
